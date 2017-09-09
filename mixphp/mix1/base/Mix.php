c076d9d430722f1035769b34b422cada0fad734d
LiuJian <onanying@126.com>
2017-09-09 10:34:42 +0800

    完善mix

--- mixphp/mix1/base/Mix.php
+++ mixphp/mix1/base/Mix.php
@@ -4,33 +4,31 @@
  * Mix类
  * @author 刘健 <code.liu@qq.com>
  */
-
 class Mix
 {
 
     // App实例
-    public static $_app;
+    public static $app;
 
-    // App实例池
-    public static $apps;
-    // 主机名称
+    // 主机
     public static $host;
 
     // 返回App
     public static function app()
     {
-        // baseApp
-        if(isset(self::$_app)){
-            return self::$_app;
+        if (is_object(self::$app)) {
+            return self::$app;
         }
-        // swooleApp
-        if (isset(self::$apps[self::$host])) {
-            return self::$apps[self::$host];
+        if (is_array(self::$app)) {
+            if (isset(self::$app[self::$host])) {
+                return self::$app[self::$host];
+            }
+            if (isset(self::$app['*'])) {
+                return self::$app['*'];
+            }
+            return array_shift(self::$app);
         }
-        if (isset(self::$apps['*'])) {
-            return self::$apps['*'];
-        }
-        return array_shift(self::$apps);
+        return null;
     }
 
 }
