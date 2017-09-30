a65ed15dd7ff4842bbc00d2a26fe37a2f94d2ec6
LiuJian <onanying@126.com>
2017-09-30 11:42:32 +0800

    增加组件基类

---
+++ mixphp/mix1/base/Component.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace mix\base;
+
+/**
+ * 组件基类
+ * @author 刘健 <coder.liu@qq.com>
+ */
+class Component
+{
+
+    // 初始化事件
+    public function onInitialize()
+    {
+    }
+
+    // 开始事件
+    public function onStart()
+    {
+    }
+
+    // 结束事件
+    public function onEnd()
+    {
+    }
+
+}
