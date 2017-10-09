4c2cc14de9271baa3724ae96dfecb482997f3cf8
LiuJian <onanying@126.com>
2017-10-09 20:40:07 +0800

    修改安装提示

--- installer.php
+++ installer.php
@@ -5,9 +5,10 @@
  * @author 刘健 <coder.liu@qq.com>
  */
 
-echo 'download mixphp zip ... ';
-copy('https://github.com/mixstart/mixphp/archive/Beta3.zip', 'mixphp.zip');
-//copy('https://github.com/mixstart/mixphp/archive/master.zip', 'mixphp.zip');
+//$url = 'https://github.com/mixstart/mixphp/archive/master.zip';
+$url = 'https://github.com/mixstart/mixphp/archive/Beta3.zip';
+echo "download {$url} ... ";
+copy($url, 'mixphp.zip');
 echo 'ok' . PHP_EOL;
 
 echo 'unzip ... ';
@@ -22,11 +23,5 @@ echo 'clean temp files ... ';
 unlink('mixphp.zip');
 echo 'ok' . PHP_EOL;
 
-/*
-echo 'download composer.phar (It\'s a little slow) ... ';
-copy('https://getcomposer.org/composer.phar', __DIR__ . '/' . $dirname . 'composer.phar');
-echo 'ok' . PHP_EOL;
-*/
-
-echo 'Successfully installed in "' . __DIR__ . DIRECTORY_SEPARATOR . substr($dirname,0,strlen($dirname)-1) . '"' . PHP_EOL;
+echo 'Successfully installed in "' . __DIR__ . DIRECTORY_SEPARATOR . substr($dirname, 0, strlen($dirname) - 1) . '"' . PHP_EOL;
 unlink(__FILE__);
