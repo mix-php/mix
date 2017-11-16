32b222da74a6ddffdf0a458e25f63c4bfe41c9c3
LiuJian <onanying@126.com>
2017-11-16 10:20:06 +0800

    配置错误

--- application/index/config/main_console.php
+++ application/index/config/main_console.php
@@ -63,15 +63,15 @@ return [
         // 数据库
         'rdb'      => [
             // 类路径
-            'class'                => 'mix\rdb\Pdo',
+            'class'                           => 'mix\rdb\Pdo',
             // 数据源格式
-            'dsn'                  => 'mysql:host=127.0.0.1;port=3306;charset=utf8;dbname=test',
+            'dsn'                             => 'mysql:host=127.0.0.1;port=3306;charset=utf8;dbname=test',
             // 数据库用户名
-            'username'             => 'root',
+            'username'                        => 'root',
             // 数据库密码
-            'password'             => '',
+            'password'                        => '',
             // 设置PDO属性: http://php.net/manual/zh/pdo.setattribute.php
-            'attribute'            => [
+            'attribute'                       => [
                 // 设置默认的提取模式: \PDO::FETCH_OBJ | \PDO::FETCH_ASSOC
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
                 // 转换 NULL 和空字符串（在所有驱动中都可用，不仅限于Oracle）
@@ -80,7 +80,7 @@ return [
                 \PDO::ATTR_CASE               => \PDO::CASE_NATURAL,
             ],
             // 回滚含有零影响行数的事务
-            'rollbackZeroAffected' => false,
+            'rollbackZeroAffectedTransaction' => false,
         ],
 
         // redis
