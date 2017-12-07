c8a052c37883dd086aca93a39fcbd32a50b426ef
LiuJian <onanying@126.com>
2017-12-07 18:41:35 +0800

    目录重构

---
+++ console/websocketd/command/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace task\daemon\command;
+
+use mix\console\Controller;
+
+/**
+ * 默认控制器
+ * 这是一个单进程守护进程的范例
+ * @author 刘健 <coder.liu@qq.com>
+ */
+class IndexController extends Controller
+{
+
+    public function actionIndex()
+    {
+        // 脱离终端
+        $this->daemon();
+        // 连接redis/mysql等，使用长连接版本的数据库组件，这样组件会自动帮你维护连接不断线
+        // ...
+        // 循环执行任务
+        while (true) {
+            // 执行业务代码
+            // ...
+        }
+    }
+
+}
---
+++ console/websocketd/config/main.php
@@ -0,0 +1,94 @@
+<?php
+
+// Console应用配置
+$database = require __DIR__ . '/../../common/config/database.php';
+return [
+
+    // 基础路径
+    'basePath'            => dirname(__DIR__) . DIRECTORY_SEPARATOR,
+
+    // 控制器命名空间
+    'controllerNamespace' => 'task\daemon\command',
+
+    // 组件配置
+    'components'          => [
+
+        // 路由
+        'route'    => [
+            // 类路径
+            'class'          => 'mix\base\Route',
+            // 默认变量规则
+            'defaultPattern' => '[\w-]+',
+            // 路由变量规则
+            'patterns'       => [
+                'id' => '\d+',
+            ],
+            // 路由规则
+            'rules'          => [
+
+            ],
+        ],
+
+        // 请求
+        'request'  => [
+            // 类路径
+            'class' => 'mix\console\Request',
+        ],
+
+        // 响应
+        'response' => [
+            // 类路径
+            'class' => 'mix\console\Response',
+        ],
+
+        // 错误
+        'error'    => [
+            // 类路径
+            'class' => 'mix\console\Error',
+        ],
+
+        // 日志
+        'log'      => [
+            // 类路径
+            'class'       => 'mix\base\Log',
+            // 日志记录级别
+            'level'       => ['error', 'info', 'debug'],
+            // 日志目录
+            'logDir'      => 'logs',
+            // 日志轮转类型
+            'logRotate'   => mix\base\Log::ROTATE_DAY,
+            // 最大文件尺寸
+            'maxFileSize' => 2048 * 1024,
+        ],
+
+        // 数据库
+        'rdb'      => array_merge(
+            $database['mysql'],
+            [
+                // 类路径
+                'class'     => 'mix\client\Pdo',
+                // 设置PDO属性: http://php.net/manual/zh/pdo.setattribute.php
+                'attribute' => [
+                    // 设置默认的提取模式: \PDO::FETCH_OBJ | \PDO::FETCH_ASSOC
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                ],
+            ]
+        ),
+
+        // redis
+        'redis'    => array_merge(
+            $database['redis'],
+            [
+                // 类路径
+                'class' => 'mix\client\Redis',
+            ]
+        ),
+
+    ],
+
+    // 对象配置
+    'objects'             => [
+
+    ],
+
+];
