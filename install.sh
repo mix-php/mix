aeb5f60a039076b7fdea6485600a64cf3f92a52c
LiuJian <onanying@126.com>
2017-12-07 18:56:03 +0800

    增加 websocketd

--- install.sh
+++ install.sh
@@ -3,31 +3,40 @@
 dirname=$(cd `dirname $0`; pwd)
 
 httpd_bin="/usr/local/bin/mix-httpd"
-httpd_path="${dirname}/server/httpd/mix-httpd"
-daemon_bin="/usr/local/bin/mix-daemon"
-daemon_path="${dirname}/task/daemon/mix-daemon"
 crontab_bin="/usr/local/bin/mix-crontab"
-crontab_path="${dirname}/task/crontab/mix-crontab"
+daemon_bin="/usr/local/bin/mix-daemon"
+websocketd_bin="/usr/local/bin/mix-websocketd"
+
+httpd_path="${dirname}/server/httpd/mix-httpd"
+crontab_path="${dirname}/console/crontab/mix-crontab"
+daemon_path="${dirname}/console/daemon/mix-daemon"
+websocketd_path="${dirname}/console/websocketd/mix-websocketd"
 
 echo "rm -rf $httpd_bin"
 rm -rf $httpd_bin
-echo "rm -rf $daemon_bin"
-rm -rf $daemon_bin
 echo "rm -rf $crontab_bin"
 rm -rf $crontab_bin
+echo "rm -rf $daemon_bin"
+rm -rf $daemon_bin
+echo "rm -rf $websocketd_bin"
+rm -rf $websocketd_bin
 
 echo "ln -s $httpd_path $httpd_bin"
 ln -s $httpd_path $httpd_bin
-echo "ln -s $daemon_path $daemon_bin"
-ln -s $daemon_path $daemon_bin
 echo "ln -s $crontab_path $crontab_bin"
 ln -s $crontab_path $crontab_bin
+echo "ln -s $daemon_path $daemon_bin"
+ln -s $daemon_path $daemon_bin
+echo "ln -s $websocketd_path $websocketd_bin"
+ln -s $websocketd_path $websocketd_bin
 
 echo "chmod 777 $httpd_path"
 chmod 777 $httpd_path
-echo "chmod 777 $daemon_path"
-chmod 777 $daemon_path
 echo "chmod 777 $crontab_path"
 chmod 777 $crontab_path
+echo "chmod 777 $daemon_path"
+chmod 777 $daemon_path
+echo "chmod 777 $websocketd_path"
+chmod 777 $websocketd_path
 
 echo "Successful install to \"/usr/local/bin\""
