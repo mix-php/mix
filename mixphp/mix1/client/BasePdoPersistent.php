749b7dfb82d442a308b9d494e4b9ccfdc597e27b
LiuJian <onanying@126.com>
2018-01-19 10:24:25 +0800

    优化长连接组件为活动时间

--- mixphp/mix1/client/BasePdoPersistent.php
+++ mixphp/mix1/client/BasePdoPersistent.php
@@ -11,22 +11,25 @@ class BasePdoPersistent extends BasePdo
 
     // 连接持续时间
     public $persistentTime = 7200;
-    // 连接时间
-    protected $_connectTime;
+
+    // 最后活动时间
+    protected $_lastActiveTime;
 
     // 初始化
     protected function initialize()
     {
         // 共用连接对象
-        $hash               = md5($this->dsn . $this->username . $this->password);
-        $this->_pdo         = &\Mix::$container['pdo_' . $hash];
-        $this->_connectTime = &\Mix::$container['pdoConnectTime_' . $hash];
+        $hash                  = md5($this->dsn . $this->username . $this->password);
+        $this->_pdo            = &\Mix::$container['pdo_' . $hash];
+        $this->_lastActiveTime = &\Mix::$container['pdoLastActiveTime_' . $hash];
     }
 
     // 连接
     protected function connect()
     {
-        $this->_connectTime = time();
+        // 更新活动时间
+        $this->_lastActiveTime = time();
+        // 连接
         parent::connect();
     }
 
@@ -41,10 +44,12 @@ class BasePdoPersistent extends BasePdo
     protected function prepare()
     {
         // 主动重新连接
-        if (isset($this->_connectTime) && ($this->_connectTime + $this->persistentTime < time())) {
+        if (isset($this->_lastActiveTime) && ($this->_lastActiveTime + $this->persistentTime < time())) {
             $this->reconnect();
         }
         try {
+            // 更新活动时间
+            $this->_lastActiveTime = time();
             // 执行前准备
             parent::prepare();
         } catch (\Exception $e) {
--- mixphp/mix1/client/BaseRedisPersistent.php
+++ mixphp/mix1/client/BaseRedisPersistent.php
@@ -11,22 +11,25 @@ class BaseRedisPersistent extends BaseRedis
 
     // 连接持续时间
     public $persistentTime = 7200;
-    // 连接时间
-    protected $_connectTime;
+
+    // 最后活动时间
+    protected $_lastActiveTime;
 
     // 初始化
     public function initialize()
     {
         // 共用连接对象
-        $hash               = md5($this->host . $this->port . $this->database . $this->password);
-        $this->_redis       = &\Mix::$container['redis_' . $hash];
-        $this->_connectTime = &\Mix::$container['redisConnectTime_' . $hash];
+        $hash                  = md5($this->host . $this->port . $this->database . $this->password);
+        $this->_redis          = &\Mix::$container['redis_' . $hash];
+        $this->_lastActiveTime = &\Mix::$container['redisLastActiveTime_' . $hash];
     }
 
     // 连接
     public function connect()
     {
-        $this->_connectTime = time();
+        // 更新活动时间
+        $this->_lastActiveTime = time();
+        // 连接
         parent::connect();
     }
 
@@ -41,10 +44,12 @@ class BaseRedisPersistent extends BaseRedis
     public function __call($name, $arguments)
     {
         // 主动重新连接
-        if (isset($this->_connectTime) && ($this->_connectTime + $this->persistentTime < time())) {
+        if (isset($this->_lastActiveTime) && ($this->_lastActiveTime + $this->persistentTime < time())) {
             $this->reconnect();
         }
         try {
+            // 更新活动时间
+            $this->_lastActiveTime = time();
             // 执行命令
             return parent::__call($name, $arguments);
         } catch (\Exception $e) {
