62c50fcc6f12458b990c52d327c93ae01f1e2fce
Jian Liu <onanying@126.com>
2018-04-18 11:18:31 +0800

    修改入口

--- apps/index/public/index.php
+++ apps/index/public/index.php
@@ -6,7 +6,7 @@ define('MIX_DEBUG', true);
 define('MIX_ENV', 'dev');
 
 require __DIR__ . '/../../../vendor/autoload.php';
-require __DIR__ . '/../../../mixphp/mix1/Mix.php';
+require __DIR__ . '/../../../vendor/mixstart/framework/Mix.php';
 
 $config = require __DIR__ . '/../config/main_traditional.php';
 (new mix\web\Application($config))->run();
