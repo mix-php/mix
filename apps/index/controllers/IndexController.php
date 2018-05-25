5d30e15b8b6806ec2b46e7f29431b4fd8e360bec
Jian Liu <onanying@126.com>
2018-05-25 17:46:58 +0800

    修改DEMO

--- apps/index/controllers/IndexController.php
+++ apps/index/controllers/IndexController.php
@@ -2,9 +2,7 @@
 
 namespace apps\index\controllers;
 
-use mix\facades\Request;
 use mix\http\Controller;
-use apps\index\models\IndexForm;
 
 /**
  * 默认控制器
@@ -17,37 +15,6 @@ class IndexController extends Controller
     public function actionIndex()
     {
         return 'Hello World' . PHP_EOL;
-    }
-
-    // API 范例，访问路径：/index/api
-    public function actionApi()
-    {
-        // 使用模型
-        $model             = new IndexForm();
-        $model->attributes = Request::get() + Request::post();
-        $model->setScenario('test');
-        if (!$model->validate()) {
-            return ['code' => 1, 'message' => '参数格式效验失败', 'data' => $model->getErrors()];
-        }
-        $model->save();
-        // 响应
-        app()->response->format = \mix\http\Response::FORMAT_JSON;
-        return ['code' => 0, 'message' => 'OK'];
-    }
-
-    // WebSite 范例，访问路径：/index/website
-    public function actionWebsite()
-    {
-        // 使用模型
-        $model             = new IndexForm();
-        $model->attributes = Request::get() + Request::post();
-        $model->setScenario('test');
-        if (!$model->validate()) {
-            return $this->render('web_site_example', ['message' => $model->getError()]);
-        }
-        $model->save();
-        // 响应
-        return $this->render('web_site_example', ['message' => '新增成功']);
     }
 
 }
---
+++ apps/index/controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace apps\index\controllers;
+
+use apps\index\models\UserForm;
+use mix\facades\Request;
+use mix\http\Controller;
+
+class UserController extends Controller
+{
+
+    public function actionCreate()
+    {
+        app()->response->format = \mix\http\Response::FORMAT_JSON;
+
+        // 使用模型
+        $model             = new UserForm();
+        $model->attributes = Request::get() + Request::post();
+        $model->setScenario('create');
+        if (!$model->validate()) {
+            return ['code' => 1, 'message' => 'FAILED', 'data' => $model->getErrors()];
+        }
+
+        // 执行保存数据库
+        // ...
+
+        // 响应
+        return ['code' => 0, 'message' => 'OK'];
+    }
+
+}
