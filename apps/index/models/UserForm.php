868b8cef60ed1100732554126dacce50b34e5b52
Jian Liu <onanying@126.com>
2018-05-25 20:08:43 +0800

    修改验证提示

--- apps/index/models/UserForm.php
+++ apps/index/models/UserForm.php
@@ -35,7 +35,7 @@ class UserForm extends Validator
         return [
             'name.required'  => '名称不能为空.',
             'name.maxLength' => '名称最多不能超过25个字符.',
-            'age.type'       => '年龄必须是数字.',
+            'age.integer'    => '年龄必须是数字.',
             'age.unsigned'   => '年龄不能为负数.',
             'age.min'        => '年龄不能小于1.',
             'age.max'        => '年龄不能大于120.',
