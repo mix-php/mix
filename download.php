0ce6f25912e3ac46e4aee10d55e30416a94f9055
Jian Liu <onanying@126.com>
2018-06-20 19:47:30 +0800

    修改为最新版本

--- download.php
+++ download.php
@@ -5,7 +5,7 @@
  * @author 刘健 <coder.liu@qq.com>
  */
 
-$url     = 'https://github.com/mixstart/mixphp/releases/download/v1.0.11/mixphp-v1.0.11.zip';
+$url     = 'https://github.com/mixstart/mixphp/releases/download/v1.0.15/mixphp-v1.0.15.zip';
 $zipfile = basename($url);
 
 // 下载
