e624c66a36d784edd34d4a8a2628fb5e7ccc444f
Jian Liu <onanying@126.com>
2018-08-21 11:49:21 +0800

    增加上传文件demo

---
+++ apps/index/controllers/FileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace apps\index\controllers;
+
+use apps\index\models\FileForm;
+use mix\facades\Request;
+use mix\http\Controller;
+
+class FileController extends Controller
+{
+
+    // 文件上传
+    public function actionUpload()
+    {
+        app()->response->format = \mix\http\Response::FORMAT_JSON;
+
+        // 使用模型
+        $model             = new FileForm();
+        $model->attributes = Request::post();
+        $model->setScenario('upload');
+        if (!$model->validate()) {
+            return ['code' => 1, 'message' => 'FAILED', 'data' => $model->getErrors()];
+        }
+
+        // 保存文件
+        $filename = app()->getPublicPath() . '/uploads/' . date('Ymd') . '/' . $model->file->getRandomName();
+        $model->file->saveAs($filename);
+
+        // 响应
+        return ['code' => 0, 'message' => 'OK'];
+    }
+
+}
