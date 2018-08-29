5a28a9c1949f0d5671ab7074a4fb5ef5bc4e0efa
Jian Liu <onanying@126.com>
2018-08-29 12:02:28 +0800

    修改为遍历

--- install.sh
+++ install.sh
@@ -1,42 +1,24 @@
 #!/bin/bash
 
+function getdir() {
+    for element in `ls $1`
+    do
+        file=$1"/"$element
+        if [ -f $file ]
+        then
+            bin_path=$1"/"$element
+            sysbin_path="/usr/local/bin/"$element
+            echo "chmod 777 $bin_path"
+            chmod 777 $bin_path
+            echo "rm -rf $sysbin_path"
+            rm -rf $sysbin_path
+            echo "ln -s $bin_path $sysbin_path"
+            ln -s $bin_path $sysbin_path
+        fi
+    done
+}
+
 dirname=$(cd `dirname $0`; pwd)
-
-httpd_bin="/usr/local/bin/mix-httpd"
-crontab_bin="/usr/local/bin/mix-crontab"
-daemon_bin="/usr/local/bin/mix-daemon"
-websocketd_bin="/usr/local/bin/mix-websocketd"
-
-httpd_path="${dirname}/bin/mix-httpd"
-crontab_path="${dirname}/bin/mix-crontab"
-daemon_path="${dirname}/bin/mix-daemon"
-websocketd_path="${dirname}/bin/mix-websocketd"
-
-echo "rm -rf $httpd_bin"
-rm -rf $httpd_bin
-echo "rm -rf $crontab_bin"
-rm -rf $crontab_bin
-echo "rm -rf $daemon_bin"
-rm -rf $daemon_bin
-echo "rm -rf $websocketd_bin"
-rm -rf $websocketd_bin
-
-echo "ln -s $httpd_path $httpd_bin"
-ln -s $httpd_path $httpd_bin
-echo "ln -s $crontab_path $crontab_bin"
-ln -s $crontab_path $crontab_bin
-echo "ln -s $daemon_path $daemon_bin"
-ln -s $daemon_path $daemon_bin
-echo "ln -s $websocketd_path $websocketd_bin"
-ln -s $websocketd_path $websocketd_bin
-
-echo "chmod 777 $httpd_path"
-chmod 777 $httpd_path
-echo "chmod 777 $crontab_path"
-chmod 777 $crontab_path
-echo "chmod 777 $daemon_path"
-chmod 777 $daemon_path
-echo "chmod 777 $websocketd_path"
-chmod 777 $websocketd_path
+getdir "$dirname/bin"
 
 echo "Successful install to \"/usr/local/bin\""
