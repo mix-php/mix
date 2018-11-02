21d57dda8050a0cc9d4309bff74607241788a2d5
Jian Liu <onanying@126.com>
2018-11-02 18:28:33 +0800

    修改安装脚本

--- install.sh
+++ install.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-function lndir() {
-    for element in `ls $1`
-    do
-        file=$1"/"$element
-        if [ -f $file ]
-        then
-            bin_path=$1"/"$element
-            sysbin_path="/usr/local/bin/"$element
-            echo "chmod 777 $bin_path"
-            chmod 777 $bin_path
-            echo "rm -rf $sysbin_path"
-            rm -rf $sysbin_path
-            echo "ln -s $bin_path $sysbin_path"
-            ln -s $bin_path $sysbin_path
-        fi
-    done
-}
-
-dirname=$(cd `dirname $0`; pwd)
-lndir "$dirname/bin"
+for element in `find ./ -name mix-*`
+do
+    element=${element:2}
+    dirname=$(cd `dirname $0`; pwd)
+    file=$(basename $element)
+    bin_path="$dirname/$element"
+    sysbin_path="/usr/local/bin/"$file
+    echo "chmod 777 $bin_path"
+    chmod 777 $bin_path
+    echo "rm -rf $sysbin_path"
+    rm -rf $sysbin_path
+    echo "ln -s $bin_path $sysbin_path"
+    ln -s $bin_path $sysbin_path
+done
 
 echo "Successful install to \"/usr/local/bin\""
