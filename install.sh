4c0c4b662c45edd89a6303b6568b14828d63a560
Jian Liu <onanying@126.com>
2018-11-09 16:47:22 +0800

    重写

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+install_sys() {
+    for element in `find ./ -type f -name mix-*`
+    do
+        element=${element:2}
+        dirname=$(cd `dirname $0`; pwd)
+        file=$(basename $element)
+        bin_path="$dirname/$element"
+        ln_bin_path="/usr/local/bin/"$file
+        echo "chmod 777 $bin_path"
+        chmod 777 $bin_path
+        echo "rm -rf $ln_bin_path"
+        rm -rf $ln_bin_path
+        echo "ln -s $bin_path $ln_bin_path"
+        ln -s $bin_path $ln_bin_path
+    done
+    echo "Successful install to \"/usr/local/bin\""
+}
+
+install_work() {
+    if [ ! -d "./bin/" ]
+    then
+        mkdir ./bin
+    fi
+    for element in `find ./ -type f -name mix-*`
+    do
+        element=${element:2}
+        dirname=$(cd `dirname $0`; pwd)
+        file=$(basename $element)
+        bin_path="$dirname/$element"
+        ln_bin_path="./bin/"$file
+        echo "chmod 777 $bin_path"
+        chmod 777 $bin_path
+        echo "rm -rf $ln_bin_path"
+        rm -rf $ln_bin_path
+        echo "ln -s $bin_path $ln_bin_path"
+        ln -s $bin_path $ln_bin_path
+    done
+    echo "Successful install to \"./bin\""
+}
+
+case "$1" in
+    work)
+        install_work
+        ;;
+    sys)
+        install_sys
+        ;;
+    *)
+        install_work
+        ;;
+esac
