a545ef85d6054fce89b6e2d695fe60543d52fc98
Jian Liu <onanying@126.com>
2018-12-04 10:40:46 +0800

    增加app接口

--- apps/common/src/Core/ApplicationInterface.php
+++ apps/common/src/Core/ApplicationInterface.php
@@ -6,6 +6,7 @@ namespace Apps\Common\Core;
  * App接口
  * @author 刘健 <coder.liu@qq.com>
  *
+ * 系统组件
  * @property \Mix\Log\Logger $log
  * @property \Mix\Console\Input $input
  * @property \Mix\Console\Output $output
@@ -16,12 +17,14 @@ namespace Apps\Common\Core;
  * @property \Mix\Http\Token $token
  * @property \Mix\Http\Session $session
  * @property \Mix\Http\Cookie $cookie
- * @property \Mix\Database\PDOConnection $pdo
- * @property \Mix\Redis\RedisConnection $redis
  * @property \Mix\WebSocket\TokenReader $tokenReader
  * @property \Mix\WebSocket\SessionReader $sessionReader
  * @property \Mix\WebSocket\MessageHandler $messageHandler
+ * 
+ * 用户自定义组件
  * @property \Mix\Redis\Coroutine\RedisPool $redisPool
+ * @property \Mix\Database\PDOConnection $pdo
+ * @property \Mix\Redis\RedisConnection $redis
  */
 interface ApplicationInterface
 {
