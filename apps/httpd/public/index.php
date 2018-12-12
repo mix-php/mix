0ba2c1941b3d070c420b4ac6132ab8b1cbe366cc
Jian Liu <onanying@126.com>
2018-12-12 19:08:05 +0800

    环境获取重写

--- apps/httpd/public/index.php
+++ apps/httpd/public/index.php
@@ -4,7 +4,7 @@
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
-Mix\Core\Env::load(__DIR__ . '/../../../.env');
+Mix\Config\Environment::load(__DIR__ . '/../../../.env');
 
 $config = require __DIR__ . '/../config/http_compatible.php';
 (new Mix\Http\Application($config))->run();
