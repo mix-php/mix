d25ab4f956c7d03367e26f5cd34a972e5fbd34c9
Jian Liu <onanying@126.com>
2019-01-22 11:30:50 +0800

    修改格式

--- applications/httpd/public/index.php
+++ applications/httpd/public/index.php
@@ -3,8 +3,6 @@
 // web入口文件
 
 require __DIR__ . '/../../../vendor/autoload.php';
-
 Mix::loadEnvironmentFrom(__DIR__ . '/../../../.env');
-
 $config = require __DIR__ . '/../config/http_compatible.php';
 (new Mix\Http\Application($config))->run();
