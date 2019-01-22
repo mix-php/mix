b4779f0e81e6045f79c8c11f044e858cc2aa9868
Jian Liu <onanying@126.com>
2019-01-22 11:26:14 +0800

    修改环境配置方法

--- applications/httpd/public/index.php
+++ applications/httpd/public/index.php
@@ -4,7 +4,7 @@
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
-Mix\Config\Environment::load(__DIR__ . '/../../../.env');
+Mix::loadEnvironmentFrom(__DIR__ . '/../../../.env');
 
 $config = require __DIR__ . '/../config/http_compatible.php';
 (new Mix\Http\Application($config))->run();
