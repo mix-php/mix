a904425098bd0ce4e3b7e4dcec9c3d8c9918ab3e
刘健 <onanying@126.com>
2019-03-05 23:02:46 +0800

    更新

--- applications/common/src/ApplicationInterface.php
+++ applications/common/src/ApplicationInterface.php
@@ -11,13 +11,13 @@
  * @property \Mix\Http\Message\Response|\Mix\Http\Message\Compatible\Response $response
  * @property \Mix\Console\Error|\Mix\Http\Error|\Mix\WebSocket\Error $error
  * @property \Mix\WebSocket\Registry $registry
- * @property \Mix\WebSocket\Session\WebSocketSession $wsSession
  * @property \Mix\WebSocket\WebSocketConnection $ws
  * @property \Mix\WebSocket\Frame $frame
  *
  * 自定义组件
  * @property \Mix\Auth\Authorization $auth
  * @property \Mix\Http\Session\HttpSession $session
+ * @property \Mix\WebSocket\Session\WebSocketSession $wsSession
  * @property \Mix\Database\PDOConnection|Mix\Database\MasterSlave\PDOConnection $pdo
  * @property \Mix\Redis\RedisConnection $redis
  * @property \Mix\Database\Pool\ConnectionPool $pdoPool
