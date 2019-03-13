8f2ade6bbe2067ddd5183a0061aefc918dd25287
Jian Liu <onanying@126.com>
2019-03-13 14:26:38 +0800

    完善ws的demo

--- applications/websocket/src/Controllers/JoinController.php
+++ applications/websocket/src/Controllers/JoinController.php
@@ -2,6 +2,7 @@
 
 namespace WebSocket\Controllers;
 
+use Mix\Core\Coroutine\Timer;
 use Mix\Helper\JsonHelper;
 use Mix\Redis\Coroutine\RedisConnection;
 use Mix\WebSocket\Frame\TextFrame;
@@ -34,36 +35,42 @@ class JoinController
         app()->tcpSession->set('roomid', $model->roomid);
 
         // 重复加入处理
-        if ($subConn = app()->tcpSession->get('subconn')) {
-            /**
-             * @var \Mix\Redis\Coroutine\RedisConnection
-             */
+        if ($subConn = app()->tcpSession->get('subConn')) {
+            /** @var \Mix\Redis\Coroutine\RedisConnection $subConn */
             $subConn->disconnect();
         }
 
         // 订阅房间的频道
         xgo(function () use ($model) {
+            // 订阅房间的频道
             $subConn = RedisConnection::newInstance();
             app()->tcpSession->set('subConn', $subConn);
-            $ws = app()->ws;
-            $subConn->subscribe(["room_{$model->roomid}"], function ($instance, $channel, $message) use ($ws) {
-                $frame = new TextFrame([
-                    'data' => $message,
-                ]);
-                $ws->push($frame);
-            });
+            try {
+                $subConn->subscribe(["room_{$model->roomid}"], function ($instance, $channel, $message) {
+                    $frame = new TextFrame([
+                        'data' => $message,
+                    ]);
+                    app()->ws->push($frame);
+                });
+            } catch (\Throwable $e) {
+                // disconnect() 时会导致 subscribe 抛出错误
+            }
         });
 
         // 给当前房间发送消息
-        $name    = app()->tcpSession->get('name');
-        $message = [
-            'result' => [
-                'message' => "{$name} 加入 {$model->roomid} 房间.",
-            ],
-        ];
-        $conn    = app()->redisPool->getConnection();
-        $conn->publish("room_{$model->roomid}", JsonHelper::encode($message, JSON_UNESCAPED_UNICODE));
-        $conn->release();
+        Timer::new()->after(100, function () use ($model) {
+            xgo(function () use ($model) {
+                $name    = app()->tcpSession->get('name');
+                $message = [
+                    'result' => [
+                        'message' => "{$name} 加入 {$model->roomid} 房间.",
+                    ],
+                ];
+                $conn    = app()->redisPool->getConnection();
+                $conn->publish("room_{$model->roomid}", JsonHelper::encode($message, JSON_UNESCAPED_UNICODE));
+                $conn->release();
+            });
+        });
     }
 
 }
