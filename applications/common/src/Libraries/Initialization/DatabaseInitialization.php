8d1dc792231d1467afa59e14fb366d1b1cb01632
Jian Liu <onanying@126.com>
2019-03-28 13:41:40 +0800

    增加 DatabaseInitialization

---
+++ applications/common/src/Libraries/Initialization/DatabaseInitialization.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Common\Libraries\Initialization;
+
+use Mix\Core\Application\InitializationInterface;
+
+/**
+ * Class DatabaseInitialization
+ * @package Common\Libraries\Initialization
+ * @author liu,jian <coder.keda@gmail.com>
+ */
+class DatabaseInitialization implements InitializationInterface
+{
+
+    /**
+     * 处理
+     * @return void
+     */
+    public function handle()
+    {
+        // TODO: Implement handle() method.
+    }
+
+}
