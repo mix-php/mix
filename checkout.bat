ce851e90f601fd15afca634b8a20681af8631cd4
Jian Liu <onanying@126.com>
2019-06-25 16:50:12 +0800

    增加checkout.bat

---
+++ checkout.bat
@@ -0,0 +1,111 @@
+cd vendor\mix\auth
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\bean
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\cache
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\concurrent
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\console
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\database
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\framework
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\http
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\http-daemon
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\http-message
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\http-server
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\http-session
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\log
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\middleware
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\pool
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\redis
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\server
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\tcp
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\tcp-daemon
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\tcp-server
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\tcp-session
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\udp
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\udp-daemon
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\udp-server
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\validate
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\websocket
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\websocket-daemon
+git.exe checkout %1%
+cd ../../../
+
+cd vendor\mix\websocket-server
+git.exe checkout %1%
+cd ../../../
