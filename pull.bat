328ded1ed566659afbb7b24c7a830f3c7eaf25c8
Jian Liu <onanying@126.com>
2019-06-25 16:57:27 +0800

    更新

---
+++ pull.bat
@@ -0,0 +1,111 @@
+cd vendor\mix\auth
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\bean
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\cache
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\concurrent
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\console
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\database
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\framework
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\http
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\http-daemon
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\http-message
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\http-server
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\http-session
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\log
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\middleware
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\pool
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\redis
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\server
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\tcp
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\tcp-daemon
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\tcp-server
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\tcp-session
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\udp
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\udp-daemon
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\udp-server
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\validate
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\websocket
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\websocket-daemon
+git.exe pull -v --progress "origin"
+cd ../../../
+
+cd vendor\mix\websocket-server
+git.exe pull -v --progress "origin"
+cd ../../../
