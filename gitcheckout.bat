61f24b7e16fccbcc5595361268094aac82cab5e4
Jian Liu <onanying@126.com>
2019-07-16 20:17:55 +0800

    修改库

--- gitcheckout.bat
+++ gitcheckout.bat
@@ -1,5 +1,5 @@
 @echo off
-for %%i in (auth,bean,cache,concurrent,console,database,framework,http-daemon,http-message,http-server,http-session,log,pool,redis,server,tcp,tcp-daemon,tcp-server,tcp-session,udp,udp-daemon,udp-server,validate,websocket,websocket-daemon,websocket-server) do (
+for %%i in (auth,bean,cache,concurrent,console,database,helper,http-daemon,http-message,http-server,http-session,log,pool,redis,server,tcp,tcp-daemon,tcp-server,tcp-session,udp,udp-daemon,udp-server,validate,websocket,websocket-daemon,websocket-server) do (
     echo -- %%i
     cd vendor\mix\%%i
     git.exe checkout %1%
