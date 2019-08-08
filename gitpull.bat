45a8ba5c6e4c51cd42cb0af67385419363eff10e
Jian Liu <onanying@126.com>
2019-08-08 17:56:05 +0800

    移除websocket-daemon,websocket-server

--- gitpull.bat
+++ gitpull.bat
@@ -1,5 +1,5 @@
 @echo off
-for %%i in (auth,bean,cache,concurrent,console,database,helper,http-message,http-server,http-session,log,pool,redis,server,udp-server,validate,websocket,websocket-daemon,websocket-server) do (
+for %%i in (auth,bean,cache,concurrent,console,database,helper,http-message,http-server,http-session,log,pool,redis,server,udp-server,validate,websocket) do (
     echo -- %%i
     cd vendor\mix\%%i
     git.exe pull -v --progress "origin"
