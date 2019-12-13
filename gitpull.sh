0fc39b9de8cb1cfdd96bdc8dc41b431919769ed9
LIU JIAN <onanying@126.com>
2019-12-13 17:56:15 +0800

    增加模块

--- gitpull.sh
+++ gitpull.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in auth bean cache concurrent console database event helper http-message http-server log pool redis redis-subscribe route server session validate view websocket
+for i in auth bean cache concurrent console database event helper http-message http-server log pool redis redis-subscribe route server session validate view websocket sync-invoke
 do
   echo -- $i
   cd src/$i
