4244e7429932c81ee190a64db31279cc84b07b48
LIU JIAN <onanying@126.com>
2020-01-15 14:54:46 +0800

    增加 jsonrpc

--- gitcheckout.bat
+++ gitcheckout.bat
@@ -1,5 +1,5 @@
 @echo off
-for %%i in (auth,bean,cache,concurrent,console,database,event,helper,http-message,http-server,log,pool,redis,redis-subscribe,route,server,session,validate,view,websocket,sync-invoke) do (
+for %%i in (auth,bean,cache,concurrent,console,database,event,helper,http-message,http-server,log,pool,redis,redis-subscribe,route,server,session,validate,view,websocket,sync-invoke,json-rpc) do (
     echo -- %%i
     cd src\%%i
     git.exe checkout %1%
--- gitcheckout.sh
+++ gitcheckout.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in auth bean cache concurrent console database event helper http-message http-server log pool redis redis-subscribe route server session validate view websocket sync-invoke
+for i in auth bean cache concurrent console database event helper http-message http-server log pool redis redis-subscribe route server session validate view websocket sync-invoke json-rpc
 do
   echo -- $i
   cd src/$i
