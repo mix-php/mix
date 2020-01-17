14a89040a49d6e5b0088aec4a70b101006e187e2
LIU JIAN <onanying@126.com>
2020-01-17 18:53:35 +0800

    Add split push tools

---
+++ bin/split.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+CURRENT_BRANCH="master"
+BASEPATH=$(cd `dirname $0`; cd ../src/; pwd)
+REPOS=$@
+
+function split()
+{
+    SHA1=`./bin/splitsh-lite --prefix=$1`
+    git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
+}
+
+function remote()
+{
+    git remote add $1 $2 || true
+}
+
+git pull origin $CURRENT_BRANCH
+
+if [[ $# -eq 0 ]]; then
+    REPOS=auth
+fi
+
+for REPO in $REPOS ; do
+    remote $REPO https://github.com/mix-php/$REPO.git
+
+    split "src/$REPO" $REPO
+done
