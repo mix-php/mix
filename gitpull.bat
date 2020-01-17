9f1d2e2f82da03f0a61e59f9f66a2da22962df2c
LIU JIAN <onanying@126.com>
2020-01-17 19:53:20 +0800

    remove script

--- gitpull.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for %%i in (auth,bean,cache,concurrent,console,database,event,helper,http-message,http-server,log,pool,redis,redis-subscribe,route,server,session,validate,view,websocket,sync-invoke,json-rpc) do (
-    echo -- %%i
-    cd src\%%i
-    git.exe pull -v --progress "origin"
-    cd ../../
-)
--- gitpull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for i in auth bean cache concurrent console database event helper http-message http-server log pool redis redis-subscribe route server session validate view websocket sync-invoke json-rpc
-do
-  echo -- $i
-  cd src/$i
-  git pull -v --progress "origin"
-  cd ../../
-done
