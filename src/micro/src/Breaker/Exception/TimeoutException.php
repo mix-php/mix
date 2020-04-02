cf89ea4981ad007759637d1694d145e11cacb38a
LIU JIAN <onanying@126.com>
2020-04-02 10:23:38 +0800

    feat: add timeout

---
+++ src/micro/src/Breaker/Exception/TimeoutException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mix\Micro\Breaker\Exception;
+
+/**
+ * Class TimeoutException
+ * @package Mix\Micro\Breaker\Exception
+ */
+class TimeoutException extends \RuntimeException
+{
+}
