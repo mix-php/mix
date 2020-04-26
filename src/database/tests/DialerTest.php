e82918934c5cc9115c89c8da81bd72de1d48e6e2
LIU JIAN <onanying@126.com>
2020-04-26 18:09:41 +0800

    improve: 增加单元测试

---
+++ src/database/tests/DialerTest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class DialerTest extends TestCase
+{
+
+    public function test(): void
+    {
+        $_this = $this;
+        $func  = function () use ($_this) {
+            $db     = (new \Mix\Database\Dialer())->dial(DATABASE_DSN, DATABASE_USERNAME, DATABASE_PASSWORD);
+            $conn   = $db->prepare('select * from `users` limit 1');
+            $result = $conn->queryAll();
+            $sql    = $conn->getLastSql();
+            var_dump($sql);
+            $_this->assertEquals(count($result), 1);
+        };
+        run($func);
+    }
+
+}
