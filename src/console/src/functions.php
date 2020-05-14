0f7236485e135183faa7e49041451f709bfd6130
LIU JIAN <onanying@126.com>
2020-05-14 15:34:41 +0800

    improve: alter println

--- src/console/src/functions.php
+++ src/console/src/functions.php
@@ -6,7 +6,10 @@
  */
 
 if (!function_exists('app')) {
-    // 返回App实例
+    /**
+     * 返回App实例
+     * @return \Mix\Console\Application
+     */
     function app()
     {
         return \Mix::$app;
@@ -14,7 +17,10 @@ if (!function_exists('app')) {
 }
 
 if (!function_exists('context')) {
-    // 返回Context实例
+    /**
+     * 返回Context实例
+     * @return \Mix\Bean\ApplicationContext
+     */
     function context()
     {
         return \Mix::$app->context;
@@ -22,9 +28,20 @@ if (!function_exists('context')) {
 }
 
 if (!function_exists('println')) {
-    // 输出字符串并换行
-    function println($strings)
+    /**
+     * 输出字符串并换行
+     * @param mixed ...$values
+     */
+    function println(...$values)
     {
-        echo $strings . PHP_EOL;
+        $slice = [];
+        foreach ($values as $value) {
+            if (is_scalar($value)) {
+                $slice[] = $value;
+            } else {
+                $slice[] = json_encode($value);
+            }
+        }
+        echo implode(' ', $slice) . PHP_EOL;
     }
 }
