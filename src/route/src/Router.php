a568ab81aadbba441d0b55da56de821be15a3519
LIU JIAN <onanying@126.com>
2020-06-11 19:24:28 +0800

    feat: add micro route

--- src/route/src/Router.php
+++ src/route/src/Router.php
@@ -7,6 +7,7 @@ use Mix\Http\Message\Factory\StreamFactory;
 use Mix\Http\Message\Response;
 use Mix\Http\Message\ServerRequest;
 use Mix\Http\Server\Middleware\MiddlewareDispatcher;
+use Mix\Micro\Route\RouterInterface;
 use Mix\Route\Exception\NotFoundException;
 use Psr\Http\Server\MiddlewareInterface;
 
@@ -15,7 +16,7 @@ use Psr\Http\Server\MiddlewareInterface;
  * @package Mix\Route
  * @author liu,jian <coder.keda@gmail.com>
  */
-class Router implements \Mix\Http\Server\ServerHandlerInterface
+class Router implements \Mix\Http\Server\ServerHandlerInterface, RouterInterface
 {
 
     /**
@@ -57,6 +58,52 @@ class Router implements \Mix\Http\Server\ServerHandlerInterface
     public function __construct(array $config = [])
     {
         BeanInjector::inject($this, $config);
+    }
+
+    /**
+     * 获取 url 规则映射的全部 service 名称
+     *
+     * Url                  Service
+     * /                    index
+     * /foo                 foo
+     * /foo/bar             foo
+     * /foo/bar/baz         foo
+     * /foo/bar/baz/cat     foo.bar
+     * /v1/foo/bar          v1.foo
+     * /v1/foo/bar/baz      v1.foo
+     * /v1/foo/bar/baz/cat  v1.foo.bar
+     *
+     * @return string[][] [name => [pattern,...]]
+     */
+    public function services()
+    {
+        $services = [];
+        foreach ($this->materials as $material) {
+            list($regular, , , $pattern) = $material;
+            $slice   = explode(' ', $regular);
+            $path    = substr($slice[1], 0, -3);
+            $slice   = array_filter(explode('\/', strtolower($path)));
+            $version = '';
+            if (isset($slice[1]) && stripos($slice[1], 'v') === 0) {
+                $version = array_shift($slice) . '.';
+            }
+            switch (count($slice)) {
+                case 0:
+                    $name = 'index';
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    $name = array_shift($slice);
+                    break;
+                default:
+                    array_pop($slice);
+                    array_pop($slice);
+                    $name = implode('.', $slice);
+            }
+            $services[$version . $name][] = $pattern;
+        }
+        return $services;
     }
 
     /**
