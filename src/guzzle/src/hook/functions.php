f3452b7047e321ec3bd3dbee84c842983dad31bb
LIU JIAN <onanying@126.com>
2020-07-22 18:34:08 +0800

    feat: add fopen hook

---
+++ src/guzzle/src/hook/functions.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace GuzzleHttp\Psr7;
+
+/**
+ * 重写写系统方法，使其失效
+ */
+if (!function_exists('GuzzleHttp\Psr7\set_error_handler')) {
+    function set_error_handler($call)
+    {
+    }
+}
+
+/**
+ * 重写系统方法，使其失效
+ */
+if (!function_exists('GuzzleHttp\Psr7\restore_error_handler')) {
+    function restore_error_handler()
+    {
+    }
+}
+
+/**
+ * 重写系统方法
+ */
+if (!function_exists('GuzzleHttp\Psr7\fopen')) {
+    function fopen($filename, $mode, $use_include_path = null, $context = null)
+    {
+        $fp = @\fopen($filename, $mode, $use_include_path, $context);
+        if ($fp === false) {
+            throw new \RuntimeException(sprintf('Unable to open %s using mode %s', $filename, $mode));
+        }
+        return $fp;
+    }
+}
