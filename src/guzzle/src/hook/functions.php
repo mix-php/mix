ef0acbe34c64adeaef48a026d5b94a4fd3af04f7
LIU JIAN <onanying@126.com>
2020-09-11 20:10:46 +0800

    fix: 修复hook异常

--- src/guzzle/src/hook/functions.php
+++ src/guzzle/src/hook/functions.php
@@ -26,7 +26,16 @@ if (!function_exists('GuzzleHttp\Psr7\restore_error_handler')) {
 if (!function_exists('GuzzleHttp\Psr7\fopen')) {
     function fopen($filename, $mode, $use_include_path = null, $context = null)
     {
-        $fp = @\fopen($filename, $mode, $use_include_path, $context);
+        switch (func_num_args()) {
+            case 2:
+                $fp = @\fopen($filename, $mode);
+                break;
+            case 3:
+                $fp = @\fopen($filename, $mode, $use_include_path);
+                break;
+            default:
+                $fp = @\fopen($filename, $mode, $use_include_path, $context);
+        }
         if ($fp === false) {
             throw new \RuntimeException(sprintf('Unable to open %s using mode %s', $filename, $mode));
         }
