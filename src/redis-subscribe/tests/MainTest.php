92e2932abc823f65a765c6e20043bda3ef7cac30
LIU JIAN <onanying@126.com>
2020-10-20 17:45:46 +0800

    feat: add tests

---
+++ src/redis-subscribe/tests/MainTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class MainTest extends TestCase
+{
+
+    public function testServerClient(): void
+    {
+        $_this = $this;
+        $func  = function () use ($_this) {
+            $sub = new \Mix\Redis\Subscribe\Subscriber('127.0.0.1', 6379, '', 5);
+            $sub->subscribe('foo', 'bar'); // 订阅失败将抛出异常
+            $sub->subscribe('foo1', 'bar1');
+            $sub->unsubscribe('foo', 'bar');
+
+            go(function () {
+                $redis = new \Redis();
+                $redis->connect('127.0.0.1', 6379);
+                $redis->publish('foo', 'foo');
+                $redis->publish('foo1', 'foo1');
+            });
+
+            $chan = $sub->channel();
+            while (true) {
+                $data = $chan->pop();
+                if (empty($data)) { // 手动close与redis异常断开都会导致返回false
+                    if (!$sub->closed) {
+                        // redis异常断开处理
+                        var_dump('Redis connection is disconnected abnormally');
+                    }
+                    break;
+                }
+                $this->assertEquals($data, 'foo1');
+                break;
+            }
+        };
+        run($func);
+    }
+
+}
