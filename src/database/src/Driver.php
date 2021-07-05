0bed92b36e43f2dae87cb37fefc4cec5341b0151
liujian <onanying@126.com>
2021-07-05 19:26:55 +0800

    feat:database:移除\PDO::ATTR_EMULATE_PREPARES

--- src/database/src/Driver.php
+++ src/database/src/Driver.php
@@ -46,7 +46,6 @@ class Driver
      * @var array
      */
     protected $defaultOptions = [
-        \PDO::ATTR_EMULATE_PREPARES => false,
         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
     ];
