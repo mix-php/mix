afb8af101fdd3f2b13231371d2b7de06d873ef55
liujian <onanying@126.com>
2021-09-07 11:08:53 +0800

    feat:websocket:up tests

--- src/websocket/tests/MainTest.php
+++ src/websocket/tests/MainTest.php
@@ -16,10 +16,10 @@ final class MainTest extends TestCase
             $vega->handleFunc('/websocket', function (Mix\Vega\Context $ctx) use ($upgrader) {
                 $conn = $upgrader->upgrade($ctx->request, $ctx->response);
 
-                $in = $conn->recv();
+                $in = $conn->readMessage();
                 $out = new \Swoole\WebSocket\Frame();
                 $out->data = sprintf('hello, %s', $in->data);
-                $conn->send($out);
+                $conn->writeMessage($out);
                 $conn->close();
             })->methods('GET');
 
@@ -33,8 +33,44 @@ final class MainTest extends TestCase
 
             $frame = new \Swoole\WebSocket\Frame();
             $frame->data = 'xiaoming';
-            $conn->send($frame);
-            $frame = $conn->recv();
+            $conn->writeMessage($frame);
+            $frame = $conn->readMessage();
+            $conn->close();
+
+            $_this->assertEquals($frame->data, 'hello, xiaoming');
+
+            $server->shutdown();
+        };
+        run($func);
+    }
+
+    public function __testRawServerClient(): void
+    {
+        $_this = $this;
+        $func = function () use ($_this) {
+            $upgrader = new Mix\WebSocket\Upgrader();
+
+            $server = new Swoole\Coroutine\Http\Server('0.0.0.0', 9502, false);
+            $server->handle('/websocket', function (Swoole\Http\Request $request, Swoole\Http\Response $response) use ($upgrader) {
+                $conn = $upgrader->upgradeRaw($request, $response);
+
+                $in = $conn->readMessage();
+                $out = new \Swoole\WebSocket\Frame();
+                $out->data = sprintf('hello, %s', $in->data);
+                $conn->writeMessage($out);
+                $conn->close();
+            });
+
+            go(function () use ($server) {
+                $server->start();
+            });
+
+            $conn = new \Mix\WebSocket\Client('ws://127.0.0.1:9502/websocket');
+
+            $frame = new \Swoole\WebSocket\Frame();
+            $frame->data = 'xiaoming';
+            $conn->writeMessage($frame);
+            $frame = $conn->readMessage();
             $conn->close();
 
             $_this->assertEquals($frame->data, 'hello, xiaoming');
