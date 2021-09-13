8ac43fc220a31da67a34ffd8005596b6bdc760f0
liujian <onanying@126.com>
2021-09-13 18:36:20 +0800

    feat:database:移除 \PDO::ATTR_EMULATE_PREPARES => false，严重影响压测性能

--- src/database/src/Driver.php
+++ src/database/src/Driver.php
@@ -44,7 +44,6 @@ class Driver
      */
     protected $defaultOptions = [
         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        \PDO::ATTR_EMULATE_PREPARES => false,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
     ];
 
