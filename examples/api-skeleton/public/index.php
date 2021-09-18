2d547da59a1a6d184ecc10b2a148492ac67445d5
liujian <onanying@126.com>
2021-09-18 13:47:52 +0800

    feat:examples:fpm init

--- examples/api-skeleton/public/index.php
+++ examples/api-skeleton/public/index.php
@@ -5,12 +5,15 @@ require __DIR__ . '/../vendor/autoload.php';
  * PHP-FPM, cli-server 模式专用
  */
 
+use App\Error;
 use App\Vega;
 use Dotenv\Dotenv;
+use Mix\Init\Finder;
 
 Dotenv::createUnsafeImmutable(__DIR__ . '/../', '.env')->load();
 define("APP_DEBUG", env('APP_DEBUG'));
 
-App\Error::register();
+Error::register();
+Finder::in(__DIR__ . '/../src/Container')->exec('init', 'connect');
 
 return Vega::new()->run();
