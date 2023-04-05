09633743acdb37b313c2551982c035b210a1bffb
liujian <onanying@126.com>
2023-04-05 12:58:08 +0800

    feat: update shell > bin dir

--- bin/split-linux.sh
+++ bin/split-linux.sh
@@ -9,7 +9,7 @@ REPOS=$@
 
 function split()
 {
-    SHA1=`./shell/splitsh-lite-linux --prefix=$1`
+    SHA1=`./bin/splitsh-lite-linux --prefix=$1`
     git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
 }
 
